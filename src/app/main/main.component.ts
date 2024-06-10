9ba905584e31acc0d4095e8886d7ac67322d3a5f
Dennis.B <Baust.Dennis@gmail.com>
2024-06-10 11:02:25 +0200

    about-me bare bone

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { AboutMeComponent } from '../about-me/about-me.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { HeaderComponent } from '../shared/header/header.component';
-import { SocialsComponent } from './socials/socials.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ScrollDownComponent } from './socials-scroll/scroll-down/scroll-down.component';
+import { SocialsComponent } from './socials-scroll/socials/socials.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, AboutMeComponent, SocialsComponent],
+  imports: [
+    HeaderComponent, 
+    FooterComponent, 
+    AboutMeComponent, 
+    SocialsComponent, 
+    ScrollDownComponent, 
+    SkillsComponent, 
+    PortfolioComponent, 
+    ContactComponent
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
