af00d6acab96e65a1569ee73e868add0a1e7b393
Dennis-B1988 <Baust.Dennis@gmail.com>
2025-10-16 15:20:37 +0200

    fix: redirect unknown routes to home
    - Added wildcard route
    - Redirects to root path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,6 +19,10 @@ export const routes: Routes = [
     loadComponent: () =>
       import("./imprint/imprint.component").then((m) => m.ImprintComponent),
   },
+  {
+    path: "**",
+    redirectTo: "",
+  },
 ];
 
 @NgModule({
